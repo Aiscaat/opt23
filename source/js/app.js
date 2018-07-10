a0c42772d9a8edcda909288332cdcf241a915fd3
isTerra-MaksYgso <maks.kolendyuk@gmail.com>
2018-07-10 20:19:12 +0300

    update 10.07.18

--- source/js/app.js
+++ source/js/app.js
@@ -1,4 +1,17 @@
 ;
+var $ = document;
+
 window.onload = () => {
-  console.log('1');
+   catalogSelect();
 };
+
+var catalogSelect = () => {
+   $.querySelectorAll('p.catalog-controls__item-name').forEach(item => {
+      item.onclick = () => {
+         $.querySelectorAll('.catalog-controls__select').forEach(el => {
+            if(el.classList.contains('-active') && item.nextElementSibling !== el) el.classList.remove('-active')
+         });
+         item.nextElementSibling.classList.toggle('-active');
+      }
+   })
+}
