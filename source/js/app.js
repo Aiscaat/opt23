113fe084ffb05c739eb61a9f73b77ed0a5a92dce
isTerra-maksYgso <maks.koledyuk@gmail.com>
2018-07-31 12:13:52 +0300

    add fonts, break footer, update 31.07.2018

--- source/js/app.js
+++ source/js/app.js
@@ -1,13 +1,50 @@
 ;
-var $ = document;
+/* —— total vars —— */
+var $ = document,
+   active = '-active';
+
+
 
 window.onload = () => {
    if ($.querySelector('.quick-view__wrap')) device(1);
    if ($.querySelector('.header-catalog')) device(2);
    if ($.querySelector('.product-card .product-info__feature')) productCart();
    catalogSelect();
+
+   // if ($.querySelector('.modal-form')) modalForm.open();
 };
 
+
+
+/* ————————————————— secondary functions ————————————————— */
+var _closePopupElement = (el) => {
+   el.querySelector('button.close').onclick = () => {
+      el.style.display = 'none';
+   }
+};
+
+var _tabsCommonFunc = (tabs) => {
+   var currTab = null;
+
+   tabs.forEach(item => {
+      if (item.classList.contains(active))
+         currTab = $.getElementById(item.getAttribute('href'));
+
+      item.onclick = (e) => {
+         e.preventDefault();
+         
+         if (item.classList.contains(active)) return false;
+         item.parentElement.querySelector('a.-active').classList.remove(active);
+         item.classList.add(active);
+
+         if (currTab !== null) currTab.style.display = 'none';
+
+         currTab = $.getElementById(item.getAttribute('href'));
+         currTab.style.display = 'block';
+      }
+   });
+};
+/* ————————————————— main functions ————————————————— */
 var device = (n) => {
    var windowWidth = window.innerWidth
       || document.documentElement.clientWidth
@@ -31,9 +68,79 @@ var catalogSelect = () => {
          item.nextElementSibling.classList.toggle('-active');
       }
    })
-}
+};
 
 var productCart = () => {
    var h = $.querySelector('.product-info__head').offsetHeight;
    $.querySelector('.product-card .product-info__feature').style.marginTop = `-${h-5}px`;
 };
+/* ————————————————— modules functions ————————————————— */
+
+var modalForm = (() => {
+   var form;
+
+   var init = () => {
+      form = $.querySelector('.modal-form');
+      close();
+      _tabs();
+   };
+   
+   var open = () => {
+      init();
+      form.style.display = 'flex';
+   };
+
+   var close = () => {
+      _closePopupElement(form);
+   };
+
+   var _tabs = () => {
+      var tabs = $.querySelectorAll('.modal-form__tab'),
+         contentTabs = form.querySelectorAll('form.form'),
+         recPassTab = form.querySelector('a[href="rec-pass"].form__reset-pass'),
+         tabsArr = [], currTab = null;
+
+      tabs.forEach(item => {
+         tabsArr.push(item);
+         if (item.classList.contains(active))
+            currTab = $.getElementById(item.getAttribute('href'));
+      });
+      tabsArr.push(recPassTab);      
+
+      tabsArr.forEach(item => {
+         item.onclick = (e) => {
+            e.preventDefault();
+            
+            if (item.classList.contains(active)) return false;
+            if (item !== recPassTab) {
+               item.parentElement.querySelector('a.-active').classList.remove(active);
+               item.classList.add(active);
+            } else {
+               form.querySelector('a.-active').classList.remove(active);
+
+
+               // form.querySelector('a.modal-form__tab')
+               //    .innerHTML = '1'
+               //    .classList.add(active);
+            }
+
+            currTab.style.display = 'none';
+
+            currTab = $.getElementById(item.getAttribute('href'));
+            currTab.style.display = 'block';
+         }
+      });
+
+
+      recPassTab.onclick = () => {
+         tabs[0].innerHTML = 'Назад';
+         $.getElementById('rec-pass').style.display = 'block';
+      };
+   };
+   
+   return {
+      open: open,
+      close: close,
+      init: init
+   }
+})();
