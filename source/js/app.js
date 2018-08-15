2188af31fc057892ac7bed51dcdec24fe9244fc3
isTerra-maksYgso <maks.koledyuk@gmail.com>
2018-08-15 15:28:00 +0300

    update 15.08.18 15.28

--- source/js/app.js
+++ source/js/app.js
@@ -5,16 +5,80 @@ var $ = document,
 
 
 
-window.onload = () => {
+window.addEventListener('load', () => {
    if ($.querySelector('.quick-view__wrap')) device(1);
    if ($.querySelector('.header-catalog')) device(2);
-   // if ($.querySelector('.product-card .product-info__feature')) productCart();
-   catalogSelect();
+   if ($.querySelector('.slider__wrap')) swiper({
+      elem: {
+         selector: '.slider__wrap',
+         settings: {
+            wrapperClass: "slider__list",
+            slideClass: "product",
+            slidesPerView: 'auto',
+            navigation: {
+               nextEl: '.slider__control-btn.-next',
+               prevEl: '.slider__control-btn.-prev',
+            },
+            breakpoints: {
+               540: {
+                  navigation: {
+                     nextEl: '.content-control__btn.-next',
+                     prevEl: '.content-control__btn.-prev',
+                  },
+               }
+            },
+            pagination: {
+               el: '.content-control__counter',
+               type: 'fraction'
+            }
+         }
+      }
+   });
+   if ($.querySelector('.shares__list')) swiper({
+      maxWidth: '540px',
+      elem: {
+         selector: '.shares__list',
+         settings: {
+            wrapperClass: "shares__list-wrap",
+            slideClass: "shares__item",
+            slidesPerView: 'auto',
+            navigation: {
+               nextEl: '.content-control__btn.-next',
+               prevEl: '.content-control__btn.-prev',
+            },
+            pagination: {
+               el: '.content-control__counter',
+               type: 'fraction'
+            }
+         }
+      }
+   });
+   if ($.querySelector('.advantages__list')) swiper({
+      maxWidth: '540px',
+      elem: {
+         selector: '.advantages__list',
+         settings: {
+            wrapperClass: "advantages__list-wrap",
+            slideClass: "advantages__item",
+            slidesPerView: 'auto',
+            navigation: {
+               nextEl: '.content-control__btn.-next',
+               prevEl: '.content-control__btn.-prev',
+            },
+            pagination: {
+               el: '.content-control__counter',
+               type: 'fraction'
+            }
+         }
+      }
+   });
 
+   catalogSelect();
+   
    mobileCatalogControls(); // добавить проверку на мобильное расширение
 
    // if ($.querySelector('.modal-form')) modalForm.open();
-};
+});
 
 
 
@@ -98,6 +162,36 @@ var mobileCatalogControls = () => {
 
    $.querySelector('.catalog-controls__filter-btn')
       .onclick = () => _changeDisplayVisible(filterContainer);   
+};
+
+var swiper = (obj) => {
+   var elem, slider;
+   elem = obj.elem;
+
+   var _elemCreateSlider = (elem) => 
+      slider = new Swiper(elem.selector, elem.settings);
+
+   if (!obj.minWidth && !obj.maxWidth) {
+      _elemCreateSlider(elem);
+      return false;
+   }
+
+   var media = 'all';
+
+   obj.minWidth ? media += ` and (min-width: ${obj.minWidth})` : null;
+   obj.maxWidth ? media += ` and (max-width: ${obj.maxWidth})` : null;
+
+   var breakpoint = window.matchMedia(media);
+
+   var _breakpointChecker = () => {
+      if (breakpoint.matches === true) {
+         _elemCreateSlider(elem);
+         return;
+      } else if (slider !== undefined) slider.destroy(true, true);
+   };
+
+   _breakpointChecker();
+   breakpoint.addListener(_breakpointChecker);
 };
 /* ————————————————— modules functions ————————————————— */
 
